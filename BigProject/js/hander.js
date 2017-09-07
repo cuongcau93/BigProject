bcac40b54448252e325a27386604a7d254dcf98f
cuongcau93 <cuongnm22@wru.vn>
2017-09-07 06:58:34 -0700

    update

--- BigProject/js/hander.js
+++ BigProject/js/hander.js
@@ -1,8 +1,36 @@
 
 $(document).ready(function(){
 
+
     $('.face-to-face').click(function(){
+        $(this).css('background-color', '#4c7baa');
+        $('.video-conference').css('background-color', 'white');
+        $('.teleconference').css('background-color', 'white');
+        
+        $('.note-faceToFace').show();
+        $('.note-videoConference').hide();
+        $('.note-teleconference').hide();
         
     });
+
+    $('.video-conference').click(function(){
+        $(this).css('background-color', '#4c7baa');
+        $('.face-to-face').css('background-color', 'white');
+        $('.teleconference').css('background-color', 'white');
+
+        $('.note-faceToFace').hide();
+        $('.note-videoConference').show();
+        $('.note-teleconference').hide();
+    })
+
+    $('.teleconference').click(function(){
+        $(this).css('background-color', '#4c7baa');
+        $('.face-to-face').css('background-color', 'white');
+        $('.video-conference').css('background-color', 'white');
+
+        $('.note-faceToFace').hide();
+        $('.note-videoConference').hide();
+        $('.note-teleconference').show();
+    })
 
 });
